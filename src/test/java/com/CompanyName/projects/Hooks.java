fb9ad846c5f809c3991a81bcba3b63193d911f90
sobirjon001 <sobirjon001@gmail.com>
2021-12-16 10:14:59 -0600

    implemented screenShots

--- src/test/java/com/CompanyName/projects/Hooks.java
+++ src/test/java/com/CompanyName/projects/Hooks.java
@@ -2,11 +2,20 @@ package com.CompanyName.projects;
 
 import com.CompanyName.utils.Driver;
 import io.cucumber.java.After;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 public class Hooks {
 
     @After
-    public void closeBrowser() {
+    public void tearDown(Scenario scenario) {
+        if (scenario.isFailed()) {
+            final byte[] screenshot = ((TakesScreenshot)
+                    Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot, "image/png", "pic");
+        }
         Driver.close();
     }
+
 }
